71706fce72dd71e23c2e307641ebd656c5e0b247
Lukas <pl9104@mci4me.at>
2024-04-20 09:37:29 +0200

    Footprint-all pages update

---
+++ myScript.js
@@ -0,0 +1,10 @@
+
+// Script für Suche/Filtern Table Funktion
+    $(document).ready(function(){
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+    });
+});
